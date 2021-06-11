31ba7eb8d21855c7243110052d6426a9fb61e8f2
kamelija.sekulovska1 <kamelija.sekulovska@keitaro.com>
2021-06-11 20:07:57 +0200

    Implement calculator functionality

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const buttonsData = [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '=', 'AC', 0, '.']
 
 const Calculator = () => {
+  const [value, setValue] = useState('')
+
+  const onButtonClick = (button) => {
+    if (button === '=') {
+      try {
+        setValue(eval(value))
+      } catch (e) {
+        setValue("Error")
+      }
+    }
+    else if (button === 'AC') {
+      setValue('')
+    } else {
+      setValue(`${value}${button}`)
+    }
+  }
+
   return (
     <div className="calculator-wrapper">
       <div className="calculator">
         <div className="calculator-input">
-          <input />
+          <div className="input">
+            <span>{value}</span>
+          </div>
         </div>
         <div className="calculator-buttons">
           {buttonsData.map((button, key) => (
-            <button key={key} className={typeof button === 'string' ? 'button-action' : 'button-number'}>{button}</button>
+            <button key={key}
+              onClick={() => onButtonClick(button)}
+              className={typeof button === 'string' ? 'button-action' : 'button-number'}>
+              {button}
+            </button>
           ))}
         </div>
       </div>
